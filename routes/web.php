e2304aa89604d38f14e3b81572f8f9a15d521463
sakanaction317 <danki.karate9602@gmail.com>
2024-10-31 15:51:48 +0900

    ファイルをアップロード

---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
+use App\Http\Controllers\FavoriteController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware(['auth', 'verified'])->group(function(){
+    Route::resource('products', ProductController::class);
+
+    Route::post('reviews', [ReviewController::class, 'store'])->name('reviews.store');
+
+    Route::post('favorites/{product_id}', [FavoriteController::class, 'store'])->name('favorites.store');
+    Route::delete('favorite/{product_id}', [FavoriteController::class, 'destroy'])->name('favorites.destroy');
+
+    Route::controller(UserController::class)->group(function() {
+        Route::get('users/mypage', 'mypage')->name('mypage');
+        Route::get('users/mypage/edit', 'edit')->name('mypage.edit');
+        Route::put('users/mypage', 'update')->name('mypage.update');
+    });
+});
